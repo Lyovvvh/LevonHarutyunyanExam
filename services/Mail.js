eb98188952f28ec4641ef525df8ce049f5c8a6e0
Lyovvvh <lyov207@gmail.com>
2024-09-23 21:14:26 +0400

    all project

---
+++ services/Mail.js
@@ -0,0 +1,26 @@
+import ejs from 'ejs';
+import path from 'path';
+
+
+export const sendMail = async ({to, subject, template, templateData, attachments = null}) => {
+    try {
+        const templatePath = path.resolve('./templates/email/', `${template}.ejs`);
+        const htmlData = await ejs.renderFile(templatePath, templateData);
+
+        const mailOptions = {
+            to: to,
+            from: process.env.EMAIL,
+            subject: subject,
+            html: htmlData,
+        }
+
+        if(attachments) {
+            mailOptions.attachments = attachments;
+        }
+
+        const info = await transporter.sendMail(mailOptions);
+        console.log('Mail send:' + info.response);
+    } catch (e) {
+        console.log(e);
+    }
+}
